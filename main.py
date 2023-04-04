2d4a80685404044678473482667a68a7d42704a8
Michael Avagianos <mixalisava@gmail.com>
2023-04-04 20:34:53 +0300

    Testing out flask

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
@@ -6,6 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/rnd')
+def rnd():
+    return f"{random.randint(1, 100)}"
 
 if __name__ == '__main__':
     app.run(debug=True)
